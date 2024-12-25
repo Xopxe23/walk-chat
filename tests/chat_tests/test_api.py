0739dc2fedb94415d8dc8d58846f5ef5e7a08a6e
George Dudaev <georgedudaev@MacBook-Air-George.local>
2024-12-25 12:46:42 +0300

    add tests to chats

--- tests/chat_tests/test_api.py
+++ tests/chat_tests/test_api.py
@@ -1,8 +1,50 @@
+import pytest
 from httpx import AsyncClient
+from fastapi.testclient import TestClient
+from starlette.websockets import WebSocketDisconnect
 
 
-async def test_swagger(
-    async_client: AsyncClient,
+async def test_get_my_chats(
+        async_client: AsyncClient,
+        authenticated_async_client: AsyncClient,
 ):
-    response = await async_client.get("/docs")
+    response = await async_client.get(url="/chats/my")
+    assert response.status_code == 401
+    response = await authenticated_async_client.get(url="/chats/my")
     assert response.status_code == 200
+    chats = response.json()
+    assert len(chats) == 2
+
+
+def test_ws_get_my_chats(
+        ws_authenticated_client: TestClient,
+):
+    with ws_authenticated_client.websocket_connect("chats/ws/my") as ws:
+        assert ws
+    ws_authenticated_client.headers.pop("Authorization")
+    with pytest.raises(WebSocketDisconnect):
+        with ws_authenticated_client.websocket_connect("chats/ws/my"):
+            pass
+
+
+def test_ws_get_chat_messages(
+        ws_authenticated_client: TestClient,
+):
+    chat_id = "ddf79876-07e4-4340-af35-a44daa778c19"
+    with ws_authenticated_client.websocket_connect(f"chats/ws/{chat_id}") as ws:
+        ws.send_text("Здорова брательник")
+        message = ws.receive_json()
+        assert message["chat_id"] == chat_id
+
+
+async def test_create_chat(
+        async_client: AsyncClient,
+):
+    user_ids = {
+        "user1_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "user2_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+    }
+    response = await async_client.post(url="/chats/create_chat", json=user_ids)
+    assert response.status_code == 200
+    chat = response.json()
+    assert user_ids["user1_id"] == chat["user1_id"]
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,22 @@
 import asyncio
+import json
 import os
 import sys
+import uuid
+
+from fastapi import HTTPException, status, Depends
 from typing import AsyncGenerator
 
 import pytest
 from httpx import ASGITransport, AsyncClient
-from sqlalchemy import NullPool
+from sqlalchemy import NullPool, insert
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from fastapi.testclient import TestClient
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from app.chats.models import ChatRoom
+from app.chats.filters import BaseFilter
+from app.chats.router import get_current_user_id, api_key_header
 from app.config.main import settings
 from app.database import Base, get_async_session
 from app.main import app as fastapi_app
@@ -23,7 +31,24 @@ async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
         yield session
 
 
+async def mock_get_current_user_id(token: str = Depends(api_key_header)) -> uuid.UUID:
+    if token:
+        user_id = "9c92aabb-3771-4756-97cc-b781371ff19a"
+        return uuid.UUID(user_id)
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid token",
+    )
+
+
+class FakeFilter:
+    limit: int = 10
+    offset: int = 0
+
+
 fastapi_app.dependency_overrides[get_async_session] = override_get_async_session
+fastapi_app.dependency_overrides[get_current_user_id] = mock_get_current_user_id
+fastapi_app.dependency_overrides[BaseFilter] = lambda: FakeFilter()
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -31,6 +56,16 @@ async def prepare_database():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+    def open_mock_json(model: str):
+        with open(f'tests/mocks/{model}.json', "r") as file:
+            return json.load(file)
+
+    chats = open_mock_json("chats")
+    async with async_session_maker() as session:
+        add_chats = insert(ChatRoom).values(chats)
+        await session.execute(add_chats)
+        await session.commit()
 
 
 @pytest.fixture(scope="session")
@@ -45,3 +80,27 @@ def event_loop(request):
 async def async_client():
     async with AsyncClient(transport=ASGITransport(app=fastapi_app), base_url="http://test") as async_client:
         yield async_client
+
+
+@pytest.fixture(scope="function")
+async def authenticated_async_client():
+    async with AsyncClient(transport=ASGITransport(app=fastapi_app), base_url="http://test") as async_client:
+        async_client.headers = {
+            **async_client.headers,
+            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+                             "eyJzdWIiOiI5YzkyYWFiYi0zNzcxLTQ3NTYtOTdjYy1iNzgxMzcxZmYxOWEiLCJleHAiOjE3NjYxMzQ5NzJ9."
+                             "uW6ZV8dhu7Qdjia-oiIYWwmTY5eXnZs3LtjE8GDlNqs",
+        }
+        yield async_client
+
+
+@pytest.fixture(scope="function")
+def ws_authenticated_client():
+    with TestClient(fastapi_app) as ws_client:
+        ws_client.headers = {
+            **ws_client.headers,
+            "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9."
+                             "eyJzdWIiOiI5YzkyYWFiYi0zNzcxLTQ3NTYtOTdjYy1iNzgxMzcxZmYxOWEiLCJleHAiOjE3NjYxMzQ5NzJ9."
+                             "uW6ZV8dhu7Qdjia-oiIYWwmTY5eXnZs3LtjE8GDlNqs",
+        }
+        yield ws_client
